92ab495546ad95738be6a1948163dabfea41ad7b
Ben Carter <ben.carter@justice.gov.uk>
2024-08-20 16:09:37 +0100

    fmt

--- s3.tf
+++ s3.tf
@@ -8,7 +8,7 @@ module "bucket" {
   server_side_encryption_configuration = {
     rule = {
       apply_server_side_encryption_by_default = {
-        sse_algorithm     = "aws:kms"
+        sse_algorithm = "aws:kms"
       }
     }
   }
